696a6773edf9849336c60e16c98f7621597c860a
Jose Proaño <jlps_94_11@hotmail.com>
2018-04-18 11:59:58 -0500

    feat(mains.js): Example of creating an constructor and creating a new object from it.

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+//Creando objeto data
+
+var data = {
+    color: "Rojo",
+    tamaño: "Grande",
+    marca : "Toyota",    
+    nuevo : false    
+}
+
+//Creando un constructor
+
+var auto = function(data){
+    this.color = data.color,
+    this.tamaño = data.tamaño,
+    this.marca = data.marca    
+    this.nuevo = data.nuevo
+}
+
+var miAuto = new auto(data);
+
+console.log(miAuto);
