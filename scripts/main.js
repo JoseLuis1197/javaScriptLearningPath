8213aec6bdd09b017dfc81f3312bf68c2c7b0daf
Jose Proaño <jlps_94_11@hotmail.com>
2018-04-18 12:53:03 -0500

    feat(scripts): adding prototype to the main constructor.

--- scripts/main.js
+++ scripts/main.js
@@ -12,10 +12,17 @@ var data = {
 var auto = function(data){
     this.color = data.color,
     this.tamaño = data.tamaño,
-    this.marca = data.marca    
-    this.nuevo = data.nuevo
+    this.marca = data.marca,   
+    this.nuevo = data.nuevo    
+}
+
+//Funcion de acelerar para constructor
+auto.prototype.acelerar = function(){
+    console.log("Acelerar auto " + this.marca);    
 }
 
 var miAuto = new auto(data);
 
 console.log(miAuto);
+miAuto.acelerar();
+
